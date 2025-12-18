39f75ba0cf5d2eb6c2928237b92e8a7a65ce99cd
Priscila Castellanos <milenycastell@gmail.com>
2025-12-18 16:56:19 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
- vite: {
-   plugins: [tailwindcss()],
- }
+  site: "https://priscila-castell17.github.io", // Aquí defines la URL pública de tu página
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
